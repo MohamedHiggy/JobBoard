d7eb7e67433afe0e155ba1b9ea74cd0c759a7f6a
MohamedHiggy <mohamed.higgy31@gmail.com>
2020-12-05 08:01:14 +0200

    setup client

---
+++ client/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="flex items-center justify-between p-6 bg-gray-200">
+    <nuxt-link :to="{name: 'index'}" exact-active-class="text-blue-500 font-bold">Jobs</nuxt-link>
+    <div class="flex items-center flex-wrap">
+      <span class="mx-4 font-bold">Username</span>
+      <nuxt-link :to="{name: 'index'}" class="mx-4">Mange listings</nuxt-link>
+      <nuxt-link :to="{name: 'index'}" class="mx-4">Post a job</nuxt-link>
+      <nuxt-link :to="{name: 'auth-login'}" class="mx-4" exact-active-class="text-blue-500 font-bold">Login</nuxt-link>
+      <nuxt-link :to="{name: 'index'}" class="mx-4">Logout</nuxt-link>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ client/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-gray-200">
+    <div class="text-center py-10">
+      <h1 class="text-4xl font-bold mb-2">Find the best jobs in the world.</h1>
+      <p class="text-gray-600 font-medium">We're the best job board in the entire world</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
